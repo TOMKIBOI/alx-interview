dd848e155f3f111934cdbb207e85db33c7cf91dd
TOMKIBOI <tomkiboi@yahoo.com>
2024-08-11 20:15:34 +0000

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,44 @@
+
+#!/usr/bin/python3
+"""
+Pascal triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    Returns a list representing the Pascal Triangle of n
+    Returns an empty list if n <= 0
+    """
+    triangle = []
+
+    if n <= 0:
+        return triangle
+
+    triangle.append([1])
+
+    for i in range(1, n):
+        row = [1]
+        prev_row = triangle[i - 1]
+
+        for j in range(len(prev_row) - 1):
+            row.append(prev_row[j] + prev_row[j + 1])
+
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
+
+
+def print_triangle(triangle):
+    """
+    Print the Pascal Triangle
+    """
+    for row in triangle:
+        print("[{}]".format(", ".join(map(str, row))))
+
+
+if __name__ == "__main__":
+    n = 5
+    triangle = pascal_triangle(n)
+    print_triangle(triangle)
