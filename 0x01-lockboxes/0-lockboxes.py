dbaeae536e5b847f8bdf51609bbb28d00076a86e
TOMKIBOI <tomkiboi@yahoo.com>
2024-08-15 16:35:24 +0000

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,27 @@
+
+#!/usr/bin/env python3
+"""
+Lockboxes function
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Determines whether all locked boxes can be opened with the keys
+    available.
+    """
+    if not isinstance(boxes, list) or len(boxes) == 0:
+        return False
+
+    num_boxes = len(boxes)
+
+    for key in range(1, num_boxes):
+        box_checked = False
+        for idx in range(num_boxes):
+            if key in boxes[idx] and key != idx:
+                box_checked = True
+                break
+        if not box_checked:
+            return False
+
+    return True
