38646e41e2d2a1e77a2df0b602089af02d8a29f4
TOM <tomkiboi@yahoo.com>
2024-11-18 16:17:06 +0000

    starwars characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, body) {
+  if (error) throw error;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
+});
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (error, response, body) {
+    if (error) throw error;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
+};
